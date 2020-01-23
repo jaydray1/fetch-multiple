cf6a3aa7ead8dd22107ad85c6c4a08f3d61d8d08
JonMichael Dreher <jdreher15@gmail.com>
2020-01-23 09:00:15 -0700

    check for missing ids
    check missing ids and display error

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import "./styles.css";
 export default function App() {
   const [data, setData] = React.useState();
   // const [specificUsers, setSpecificUsers] = React.useState();
+  const [missingUsers, setMissingUsers] = React.useState();
 
   const loadData = (userIds = []) => {
+    // let appendParams = '';
+    // if (userIds.length) {
+    //   // filteredUrl = "https://jsonplaceholder.typicode.com/users?id=1&id=5"
+    //   userIds.reduce((acc, curr) => {
+    //     return acc+curr+'&';
+    //   }, 'https://jsonplaceholder.typicode.com/users?')
+    // }
     const urls = [
-      "https://jsonplaceholder.typicode.com/users",
+      "https://jsonplaceholder.typicode.com/users?id=1&id=11",
       "https://jsonplaceholder.typicode.com/posts"
     ];
 
@@ -21,7 +29,8 @@ export default function App() {
         });
         // setData(Object.assign({}, (result[0])));
         setData(result[0]);
-        console.log(data);
+        // console.log(data);
+        setMissingUsers(checkForMissingIds(result[0], [1, 11]));
 
         // const users = data.filter((user) => {
         //   const foundUser = userIds.map((found) => {
@@ -34,10 +43,32 @@ export default function App() {
       });
   };
 
+  const checkForMissingIds = (response, ids) => {
+    const missingIds = [];
+    for (let i = 0; i < ids.length; i++) {
+      const found = response.find(item => item.id === ids[i]);
+      if (found) {
+        continue;
+      } else {
+        missingIds.push(ids[i]);
+      }
+    }
+    return missingIds;
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Team</h1>
+      <h2>
+        Fetch users and posts, append posts on users, and fetch a specific list
+        of users
+      </h2>
+      {missingUsers &&
+        missingUsers.map(item => (
+          <p style={{ fontStyle: "italic" }} key={item}>
+            User: {item} could not be found, please check users
+          </p>
+        ))}
       <div
         style={{
           display: "flex",
@@ -52,11 +83,9 @@ export default function App() {
             ? data.map(el => (
                 <div key={el.name}>
                   <h3>{el.name}</h3>
-                  <p>
-                    {el.posts.map((item, index) => (
-                      <p key={index}>{item.body}</p>
-                    ))}
-                  </p>
+                  {el.posts.map((item, index) => (
+                    <p key={index}>{item.body}</p>
+                  ))}
                 </div>
               ))
             : null}
